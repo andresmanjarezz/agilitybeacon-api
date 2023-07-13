a89f4595a9d70bd3bc4dc0cf907e3cc3b0b4bca5
andresmanjarezz <one.incredy@outlook.com>
2023-07-12 23:04:01 -0700

    Fixup: Format python code with Black

--- app/api/api_v1/routers/tests/test_screens.py
+++ app/api/api_v1/routers/tests/test_screens.py
@@ -1,5 +1,6 @@
 from app.db.screens.models import Screen
 
+
 def test_get_screens(client, test_screen, superuser_token_headers):
     screen = test_screen.dict()
     response = client.get("/api/v1/screens", headers=superuser_token_headers)
@@ -7,9 +8,7 @@ def test_get_screens(client, test_screen, superuser_token_headers):
     assert all(response.json()[0][arg] == screen[arg] for arg in screen)
 
 
-def test_delete_screen(
-    client, test_screen, test_db, superuser_token_headers
-):
+def test_delete_screen(client, test_screen, test_db, superuser_token_headers):
     response = client.delete(
         f"/api/v1/screens/{test_screen.id}",
         headers=superuser_token_headers,
@@ -18,9 +17,7 @@ def test_delete_screen(
     assert test_db.query(Screen).all() == []
 
 
-def test_edit_screen(
-    client, test_screen, test_role, superuser_token_headers
-):
+def test_edit_screen(client, test_screen, test_role, superuser_token_headers):
     new_screen = {
         "name": "New screens",
         "description": "New desc",
@@ -32,7 +29,4 @@ def test_edit_screen(
         headers=superuser_token_headers,
     )
     assert response.status_code == 200
-    assert all(
-        response.json()[arg] == new_screen[arg] for arg in new_screen
-    )
-
+    assert all(response.json()[arg] == new_screen[arg] for arg in new_screen)

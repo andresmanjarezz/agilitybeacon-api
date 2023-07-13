8f15eeb9845c37616d429a1edfdf7ff4593aa7be
andresmanjarezz <one.incredy@outlook.com>
2023-07-12 23:04:11 -0700

    Remove role reference from other models on delete

--- app/db/roles/crud.py
+++ app/db/roles/crud.py
@@ -1,11 +1,20 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.attributes import flag_modified
+from app.db.use_cases.models import UseCase
+from app.db.playbooks.models import Playbook
+from app.db.jobs.models import Job
 
 
 def delete_role_mapping(db: Session, role_id: int):
-    # To Do: Delete job mapping
 
-    # To Do: Delete use case mapping
+    # Remove role reference from other models
+    affected_models = [Job, UseCase, Playbook]
 
-    # To Do: Delete playbook mapping
-
-    pass
+    for model in affected_models:
+        items = db.query(model).filter(model.role_ids.any(role_id)).all()
+        for item in items:
+            item.role_ids.remove(role_id)
+            flag_modified(item, "role_ids")
+            db.merge(item)
+            db.flush()
+            db.commit()
